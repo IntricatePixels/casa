33c358a4b9543469ab22c5e926a680f0e5d178e7
Taulant Balla <taulant.balla@gmail.com>
2024-06-17 00:43:27 +0200

    Updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,15 +1,36 @@
-@php $mimeo_current_lang = get_field('site_id', 'option'); @endphp
-@include('sections.header')
+<!doctype html>
+<html @php(language_attributes())>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @php(do_action('get_header'))
+    @php(wp_head())
+  </head>
 
-<main id="main" class="main">
-    @yield('content')
-</main>
+  <body @php(body_class())>
+    @php(wp_body_open())
 
-@hasSection('sidebar')
-    <aside class="sidebar">
-        @yield('sidebar')
-    </aside>
-@endif
-@include('sections.footer')
-@include('sections.mobile-menu')
-@include ('sections.login-menu')
+    <div id="app">
+      <a class="sr-only focus:not-sr-only" href="#main">
+        {{ __('Skip to content', 'sage') }}
+      </a>
+
+      @include('sections.header')
+
+      <main id="main" class="main">
+        @yield('content')
+      </main>
+
+      @hasSection('sidebar')
+        <aside class="sidebar">
+          @yield('sidebar')
+        </aside>
+      @endif
+
+      @include('sections.footer')
+    </div>
+
+    @php(do_action('get_footer'))
+    @php(wp_footer())
+  </body>
+</html>
