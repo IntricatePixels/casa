01504b297cbd367833590f338c1ac4313d0dcde3
Taulant Balla <taulant.balla@gmail.com>
2024-06-17 08:38:45 +0200

    Footer updates

--- resources/views/partials/footer-big.blade.php
+++ resources/views/partials/footer-big.blade.php
@@ -1,6 +1,6 @@
 <footer class="pt-0 bg-casa-light">
     <img src="@asset('images/map_lago.jpg')" alt="Map" width="1000" height="500" class="object-fit" style="height: 500px;">
-    <div class="container mt-4">
+    <div class="container mt-4 pt-4">
         <div class="row">
             <!-- Left Column for WordPress Menu -->
             <div class="col-lg-3">
@@ -13,22 +13,20 @@
             <!-- Right Column for Newsletter Signup and Social Media Icons -->
             <div class="col-lg-9 d-flex justify-content-end flex-column align-items-end">
                 <div class="card border-0 bg-transparent">
-                    <div class="card-body">
                         <h3>Newsletter</h3>
                         <!-- Replace with your Contact Form 7 shortcode -->
                         <?php echo do_shortcode('[contact-form-7 id="eb7b80d" title="Newsletter Sign Up"]'); ?>
-                    </div>
-                </div>
-<br>
-                <div class="mt-3">
+                        <div class="mt-3">
                     <!-- Social Media Icons with Links -->
-                    <a href="https://facebook.com/your-page" class="btn btn-outline-primary me-2">
+                    <a href="https://facebook.com/your-page" class="btn btn-outline-dark me-2">
                         <i class="bi bi-facebook"></i>
                     </a>
-                    <a href="https://instagram.com/your-page" class="btn btn-outline-primary">
+                    <a href="https://instagram.com/your-page" class="btn btn-outline-dark">
                         <i class="bi bi-instagram"></i>
                     </a>
                 </div>
+                </div>
+                
             </div>
         </div>
     </div>
