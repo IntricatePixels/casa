c9474205c646f6adbc69c3c2038f1d46ff1a80b6
Taulant Balla <taulant.balla@gmail.com>
2024-06-27 07:31:58 +0200

    Fix gallery id

--- resources/views/partials/newblocks/new-block-image-gallery-slider.blade.php
+++ resources/views/partials/newblocks/new-block-image-gallery-slider.blade.php
@@ -6,7 +6,7 @@
     
     @if ($gallery_images)
         <section class="block py-5 my-5" id="imageSlider">
-            <div id="carouselExampleFade" class="carousel carousel-dark slide" data-bs-ride="false" data-bs-pause="false">
+            <div id="carouselSlideGallery" class="carousel carousel-dark slide" data-bs-ride="false" data-bs-pause="false">
                 <div class="carousel-inner">
                     @foreach ($gallery_images as $index => $image)
                         @php
@@ -14,17 +14,18 @@
                             $active_class = ($index == 0) ? 'active' : '';
                         @endphp
                         <div class="carousel-item {{ $active_class }}">
+                        
                             <div class="d-flex justify-content-center align-items-center">
                                 <img src="{{ $image_url }}" class="d-block" style="max-width: 100%; max-height: 500px; object-fit: contain;" alt="Slide Image">
                             </div>
                         </div>
                     @endforeach
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselSlideGallery" data-bs-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Previous</span>
                 </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselSlideGallery" data-bs-slide="next">
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Next</span>
                 </button>
